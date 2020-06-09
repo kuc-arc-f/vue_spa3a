3feb02da9ed0efc2382dd2471df88adaa625e511
中島浩二 <knaka0209@gmail.com>
2020-06-09 09:50:01 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,36 @@
+﻿# vue_spa3a
+
+ Version: 0.9.1
+
+ Author  : Kouji Nakashima / kuc-arc-f.com
+
+ date    : 2020/06/08
+
+***
+### Summary
+Vue CLI + Laravel SPA sample
+
+* PWA 対応
+
+***
+### Setup
+
+* npm install -g @vue/cli
+
+* vue create my-project
+
+* cd my-project
+
+* start server
+
+npm run serve
+
+* build project, deploy
+
+npm run build
+
+***
+### Blog
+
+***
+
---
+++ src/router/index.js
@@ -0,0 +1,28 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+
+import home from '../components/Home'
+import about from '../components/About'
+
+import tasksIndex from '../components/Tasks/Index'
+import tasksNew from '../components/Tasks/new'
+import tasksTest from '../components/Tasks/test'
+import tasksShow from '../components/Tasks/show'
+import tasksEdit from '../components/Tasks/edit'
+
+//
+Vue.use(Router)
+//
+export default new Router({
+	/* mode: 'history', */
+	routes: [
+		{ path: '/', component: home },
+		{ path: '/about', component: about },
+		/* todos */
+		{ path: '/tasks/edit/:id', component: tasksEdit },
+		{ path: '/tasks', component: tasksIndex },
+		{ path: '/tasks/new', component: tasksNew },
+		{ path: '/tasks/test', component: tasksTest },
+		{ path: '/tasks/show/:id', component: tasksShow },
+	]
+})
